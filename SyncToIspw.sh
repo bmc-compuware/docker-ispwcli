9a4a11fc41df29dc3d22417356ec04630d48086f
zhouqr2000 <zhouqr2000@gmail.com>
2021-04-12 10:02:24 -0400

    update to match tag version while publish

--- SyncToIspw.sh
+++ SyncToIspw.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#/**
+# * These materials contain confidential information and trade secrets of Compuware Corporation. You shall maintain the materials
+# * as confidential and shall not disclose its contents to any third party except as may be required by law or regulation. Use,
+# * disclosure, or reproduction is prohibited without the prior express written permission of Compuware Corporation.
+# * 
+# * All Compuware products listed within the materials are trademarks of Compuware Corporation. All other company or product
+# * names are trademarks of their respective owners.
+# * 
+# * Copyright (c) 2021 Compuware Corporation. 
+# * Copyright (c) 2021 BMC Software, Inc. All rights reserved.
+# */
 
 echo "INPUT_SHOWENV=${INPUT_SHOWENV}"
 echo "INPUT_ENCRYPTIONPROTOCOL=${INPUT_ENCRYPTIONPROTOCOL}"
